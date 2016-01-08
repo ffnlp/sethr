b5ce2650411019d03fac147ab5d42f01cab1e125
Nikola Ljubešić <nljubesi@gmail.com>
2016-01-08 11:41:50 +0000

    Minor interventions

--- scripts/apply_mte4r-upos.mapping.py
+++ scripts/apply_mte4r-upos.mapping.py
@@ -3,12 +3,15 @@ import sys
 map=dict([(a,c) for a,b,c,d in [e.strip().split('\t') for e in open('../mte4r-upos.mapping')]])
 
 for path in ['../set.hr.conll','../web.hr.conll','../news.hr.conll','../set.hr.test.conll','../set.sr.test.conll','../wiki.hr.test.conll','../wiki.sr.test.conll','../web.hr.test.conll']:
-  for line in open(path).readlines():
+  lines=open(path).readlines()
+  out=open(path,'w')
+  for line in lines:
     if line.strip()=='':
-      sys.stdout.write('\n')
+      out.write('\n')
     else:
       line=line.split('\t')
       line[5]=map[line[4]]
-      sys.stdout.write('\t'.join(line))
+      out.write('\t'.join(line))
+  out.close()
   print path,'done'
   
